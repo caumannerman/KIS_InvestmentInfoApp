96bc84b3c2ab0eaba5d6e977b9294acf9ec75d67
Yang <75043852+caumannerman@users.noreply.github.com>
2023-01-02 13:30:03 +0900

    ButtonListView collectionView 구현

--- KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/ShowDataView/SubComponents/ButtonList/ButtonListView.swift
+++ KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/ShowDataView/SubComponents/ButtonList/ButtonListView.swift
@@ -9,3 +9,17 @@ import UIKit
 import SnapKit
 
 
+final class ButtonListView: UICollectionView {
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        
+        self.register(ButtonListViewCell.self, forCellWithReuseIdentifier: "ButtonListViewCell")
+        self.backgroundColor = .green
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
