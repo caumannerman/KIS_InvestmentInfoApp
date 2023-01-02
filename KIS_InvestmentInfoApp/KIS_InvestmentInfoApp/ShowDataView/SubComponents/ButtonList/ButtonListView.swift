441e0de41c1846ced7052cfc78bb35f313023fa7
Yang <75043852+caumannerman@users.noreply.github.com>
2023-01-02 13:21:54 +0900

    API data ButtonListViewCell 구현

--- KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/ShowDataView/SubComponents/ButtonList/ButtonListView.swift
+++ KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/ShowDataView/SubComponents/ButtonList/ButtonListView.swift
@@ -5,4 +5,7 @@
 //  Created by 양준식 on 2023/01/02.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+
--- KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/ShowDataView/SubComponents/ButtonList/ButtonListViewCell.swift
+++ KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/ShowDataView/SubComponents/ButtonList/ButtonListViewCell.swift
@@ -5,4 +5,32 @@
 //  Created by 양준식 on 2023/01/02.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class ButtonListViewCell: UICollectionViewCell{
+    private lazy var titleLabel: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 13.0, weight: .bold)
+        button.backgroundColor = .cyan
+        button.layer.cornerRadius = 12.0
+        
+        return button
+    }()
+    
+    func setup(title: String){
+        setupLayout()
+        titleLabel.setTitle(title, for: .normal)
+    }
+}
+
+private extension ButtonListViewCell{
+    func setupLayout(){
+        [ titleLabel].forEach{ addSubview($0)}
+        
+        titleLabel.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+    }
+}
