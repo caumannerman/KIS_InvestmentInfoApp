c9f106a84ee01603054ddf1fa4c66ef4e4364a47
Yang <75043852+caumannerman@users.noreply.github.com>
2023-01-02 11:10:21 +0900

    api show ViewController로 넘어가는 기능 구현

--- KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/HomeView/HomeViewController.swift
+++ KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/HomeView/HomeViewController.swift
@@ -12,20 +12,56 @@ class HomeViewController: UIViewController {
     //검색했던 URL들을 담을 배열
     private var urlsArr: [String] = []
     
+    private func isFirstTime() -> Bool {
+        let defaults = UserDefaults.standard
+        
+        if defaults.object(forKey: "isFirstTime") == nil {
+            defaults.set(false, forKey: "isFirstTime")
+            return true
+        } else {
+            return false
+        }
+    }
+    private let button = UIButton()
     //prefix가 같은 url들을 UserDefaults에서 불러와 뿌려줄 TableView
-    private lazy var urlTableView: UITableView = {
-        let tableView = UITableView()
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.isHidden = true
-        tableView.backgroundColor = .blue
-        return tableView
-    }()
+//    private lazy var urlTableView: UITableView = {
+//        let tableView = UITableView()
+//        tableView.dataSource = self
+//        tableView.delegate = self
+//        tableView.isHidden = true
+//        tableView.backgroundColor = .blue
+//        return tableView
+//    }()
+    private var urlTableView = UITableView()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+
+//        urlsArr = UserDefaults.standard.array(forKey: "urls") as? [String] ?? ["저장된 URL이 없음"]
+
+
+    }
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        urlTableView.dataSource = self
+        urlTableView.delegate = self
+        urlTableView.isHidden = true
+        urlTableView.backgroundColor = .blue
+        let isFT = isFirstTime()
+        print(isFT)
+        // 앱 실행이 처음이라면
+        if isFT{
+            UserDefaults.standard.set(["url1", "url2","url3","url4", "url5"], forKey: "urls")
+        }
+        print(urlsArr)
+        print(UserDefaults.standard.array(forKey: "urls") as? [String] ?? ["정보가 없습니다"])
+//        self.urlsArr = UserDefaults.standard.array(forKey: "urls") as? [String] ?? ["정보가 없습니다"]
+//        urlTableView.reloadData()
+        
+        
         view.backgroundColor = .white
         setNavigationItems()
         attribute()
@@ -50,15 +86,27 @@ class HomeViewController: UIViewController {
     }
     
     private func attribute(){
-        
+        button.setTitle("넘어가기", for: .normal)
+        button.backgroundColor = .blue
+        button.addTarget(self, action: #selector(pushVC), for: .touchUpInside)
     }
     
+    @objc func pushVC(){
+        let vc = ShowDataViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
     private func layout(){
-        [urlTableView].forEach{
+        [button, urlTableView].forEach{
             view.addSubview($0)
         }
+        button.snp.makeConstraints{
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(50)
+        }
         urlTableView.snp.makeConstraints{
-            $0.top.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.top.equalTo(button.snp.bottom)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview()
         }
     }
@@ -70,12 +118,13 @@ extension HomeViewController: UISearchBarDelegate{
         // reloadData() 해주지 않으면 이전 검색 내역이 그대로 tableView에 남아있다.
         urlTableView.reloadData()
         urlTableView.isHidden = false
+        print(urlsArr)
     }
  
     func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
         urlTableView.isHidden = true
         //나중에 다시 검색창을 켰을 때, 이전에 검색했던 지하철역들이 TableView에 그대로 보이지 않도록 리스트 비워줌
-        urlsArr = []
+        //urlsArr = []
     }
     //text가 바뀔 때마다 request를 Alamofire를 이용해 보내아고 받아옴
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
@@ -107,6 +156,7 @@ extension HomeViewController: UITableViewDataSource {
 extension HomeViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let url = urlsArr[indexPath.row]
+        print(url)
         //TODO: 여기서 채팅창에 url을 넘겨줘야함
         
     }
