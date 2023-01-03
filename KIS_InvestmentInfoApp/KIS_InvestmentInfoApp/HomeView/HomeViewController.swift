493c996cbf633c999326fc0d46399c3121006ea5
Yang <75043852+caumannerman@users.noreply.github.com>
2023-01-03 11:47:15 +0900

    SearchBar의 SearchButton 클릭 시 콜백함수를 UISearchBarDelegate에 구현

--- KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/HomeView/HomeViewController.swift
+++ KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/HomeView/HomeViewController.swift
@@ -27,36 +27,38 @@ class HomeViewController: UIViewController {
         }
     }
     
-    private lazy var addHeaderTableView: UITableView = {
-        let tableView = UITableView()
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.backgroundColor = .yellow
-        tableView.register(AddHeaderViewCell.self, forCellReuseIdentifier: "AddHeaderViewCell")
-        
-        return tableView
-    }()
-    
-    private let button = UIButton()
-    //prefix가 같은 url들을 UserDefaults에서 불러와 뿌려줄 TableView
-//    private lazy var urlTableView: UITableView = {
+//    private lazy var addHeaderTableView: UITableView = {
 //        let tableView = UITableView()
-//        tableView.dataSource = self
-//        tableView.delegate = self
-//        tableView.isHidden = true
-//        tableView.backgroundColor = .blue
+////        tableView.dataSource = self
+////        tableView.delegate = self
+//        tableView.backgroundColor = .yellow
+//        tableView.register(AddHeaderViewCell.self, forCellReuseIdentifier: "AddHeaderViewCell")
+//        tableView.rowHeight = 50
+//        tableView.tag = 1
 //        return tableView
 //    }()
+    
+//    private let button = UIButton()
+    //prefix가 같은 url들을 UserDefaults에서 불러와 뿌려줄 TableView
+
     private lazy var urlTableView: UITableView = {
         let tableView = UITableView()
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.isHidden = true
+//        tableView.isHidden = true
         tableView.backgroundColor = .blue
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "urlTableViewCel")
-        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "urlTableViewCell")
+        tableView.tag = 2
         return tableView
         
+    }()
+    private let uiSc: UISearchController = {
+        let searchController = UISearchController()
+        searchController.searchBar.placeholder = "요청 URL을 입력해주세요"
+        //화면 어두워지지 않도록 false 처리
+        searchController.obscuresBackgroundDuringPresentation = false
+        
+        return searchController
     }()
     
     override func viewWillAppear(_ animated: Bool) {
@@ -65,6 +67,8 @@ class HomeViewController: UIViewController {
         let ur = UserDefaults.standard.array(forKey: "urls") as? [String] ?? ["저장된 URL이 없음"]
         print(type(of: ur))
         print(ur)
+        
+     
     }
     
     
@@ -73,8 +77,7 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         // DataSource, Delegate 설정 시 구분을 위해 tag 설정
-        addHeaderTableView.tag = 1
-        urlTableView.tag = 2
+        
         
         let isFT = isFirstTime()
         print(isFT)
@@ -86,34 +89,32 @@ class HomeViewController: UIViewController {
         print(UserDefaults.standard.array(forKey: "urls") as? [String] ?? ["정보가 없습니다"])
 //        self.urlsArr = UserDefaults.standard.array(forKey: "urls") as? [String] ?? ["정보가 없습니다"]
 //        urlTableView.reloadData()
-        
-        
+
         setNavigationItems()
         attribute()
         layout()
+        
     }
     
     private func setNavigationItems(){
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "API 요청URL 입력"
         //TODO: title color 변경
-//        navigationItem.titleView?.backgroundColor = UIColor(red: 0/255.0, green: 174/255.0, blue: 194/255.0, alpha: 1.0)
-//        navigationItem.titleView?.tintColor = UIColor(red: 0/255.0, green: 174/255.0, blue: 194/255.0, alpha: 1.0)
         
-        let searchController = UISearchController()
-        searchController.searchBar.placeholder = "요청 URL을 입력해주세요"
-        //화면 어두워지지 않도록 false 처리
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.delegate = self
+//        let searchController = UISearchController()
+//        searchController.searchBar.placeholder = "요청 URL을 입력해주세요"
+//        //화면 어두워지지 않도록 false 처리
+//        searchController.obscuresBackgroundDuringPresentation = false
+        uiSc.searchBar.delegate = self
         
         // embed UISearchController
-        navigationItem.searchController = searchController
+        navigationItem.searchController = uiSc
     }
     
     private func attribute(){
-        button.setTitle("요청 URL 입력", for: .normal)
-        button.backgroundColor = .lightGray
-        button.addTarget(self, action: #selector(pushVC), for: .touchUpInside)
+//        button.setTitle("요청 URL 입력", for: .normal)
+//        button.backgroundColor = .lightGray
+//        button.addTarget(self, action: #selector(pushVC), for: .touchUpInside)
     }
     
     @objc func pushVC(){
@@ -121,24 +122,24 @@ class HomeViewController: UIViewController {
         navigationController?.pushViewController(vc, animated: true)
     }
     private func layout(){
-        [addHeaderTableView, button, urlTableView].forEach{
+        [ urlTableView].forEach{
             view.addSubview($0)
         }
-        addHeaderTableView.snp.makeConstraints{
-            $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.leading.trailing.equalToSuperview()
-            //$0.height.equalTo(100)
-        }
-        
-        button.snp.makeConstraints{
-            $0.top.equalTo(addHeaderTableView.snp.bottom)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(50)
-        }
+//        addHeaderTableView.snp.makeConstraints{
+//            $0.top.equalTo(view.safeAreaLayoutGuide)
+//
+//            $0.leading.trailing.equalToSuperview()
+//            $0.height.equalTo( (headerDict.count + 2) * 50)
+//        }
+//
+//        button.snp.makeConstraints{
+//            $0.top.equalTo(view.safeAreaLayoutGuide)
+//            $0.leading.trailing.equalToSuperview().inset(30)
+//            $0.height.equalTo(50)
+//        }
         
         urlTableView.snp.makeConstraints{
-            $0.top.equalTo(button.snp.bottom)
-            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.top.bottom.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview()
         }
     }
@@ -157,6 +158,7 @@ extension HomeViewController: UISearchBarDelegate{
         urlTableView.isHidden = true
         //나중에 다시 검색창을 켰을 때, 이전에 검색했던 지하철역들이 TableView에 그대로 보이지 않도록 리스트 비워줌
         urlsArr = []
+        
     }
     //text가 바뀔 때마다 request를 Alamofire를 이용해 보내아고 받아옴
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
@@ -167,47 +169,47 @@ extension HomeViewController: UISearchBarDelegate{
         
         requestStationName()
     }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("searchButton clicked!!")
+        print(searchBar.text)
+    }
     
 }
 
 //TableView에 대한 delegate설정
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if tableView.tag == 1{
-            return headerDict.count + 1
-        }
-        else {
             return urlsArr.count
-        }
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // addHeaderTableView
-        if tableView.tag == 1{
-            let cell = AddHeaderViewCell()
-            return cell
-        }
-               
+//        if tableView.tag == 1{
+//            guard let cell = tableView.dequeueReusableCell(withIdentifier: "AddHeaderViewCell", for: indexPath) as? AddHeaderViewCell else { return UITableViewCell() }
+//            return cell
+//        }
+
         // urlTableView
-        else {
             let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
+
             let url = urlsArr[indexPath.row]
             cell.textLabel?.text = "url별칭"
             cell.detailTextLabel?.text = url
             return cell
-        }
-       
+        
+
     }
 }
 
 extension HomeViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+     
         let url = urlsArr[indexPath.row]
         let vc = ShowDataViewController()
         vc.setup(apiUrl: url)
         navigationController?.pushViewController(vc, animated: true)
-        
+
         //TODO: 여기서 채팅창에 url을 넘겨줘야함
-        
+
     }
 }
 
