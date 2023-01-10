9c173eceff2084be49e9652ecc6ee9b0d7bc2a07
Yang <75043852+caumannerman@users.noreply.github.com>
2023-01-10 22:04:49 +0900

    json을 stack 탐색해서 만든 배열이 있을때 collectView에 뿌리기 완료

--- KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/ShowDataView/ShowDataViewController.swift
+++ KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/ShowDataView/ShowDataViewController.swift
@@ -13,33 +13,54 @@ class ShowDataViewController: UIViewController {
     
     private var apiResultStr = ""
     
+//    var records: [[String]] = (0 ..< 50).map { row in
+//        (0 ..< 6).map {
+//            column in
+//            "Row \(row) columnssssssssdfdfaddffa \(column)"
+//        }
+//    }
+    // 이것이 json String을 이용해 최종적으로 얻은 배열이라고 생각하고 개발중
+    private var jsonResultArr: [[String]] = [
+    ["col이름1", "col이름2", "col이름3", "col이름4", "col이름5", "col이름6", "col이름7", "col이름8", "col이름9", "col이름10", "col이름11", "col이름12"],
+    ["row1값1", "row1값2", "row1값3", "row1값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
+    ["row2값1", "row2값2", "row2값3", "row2값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
+    ["row3값1", "row3값2", "row3값3", "row3값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
+    ["row4값1", "row4값2", "row4값3", "row4값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
+    ["row5값1", "row5값2", "row5값3", "row5값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
+    ["row6값1", "row6값2", "row6값3", "row6값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
+    ["row7값1", "row7값2", "row7값3", "row7값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
+    ["row8값1", "row8값2", "row8값3", "row8값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
+    ["row9값1", "row9값2", "row9값3", "row9값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
+    ["row10값1", "row10값2", "row10값3", "row10값4", "row10값5", "row10값6", "row10값7", "row10값8", "row10값9", "row10값10", "row10값11", "row10값12"],
+    ["row11값1", "row11값2", "row11값3", "row11값4", "row11값5", "row11값6", "row11값7", "row11값8", "row11값9", "row11값10", "row11값11", "row11값12"],
+    ["row12값1", "row12값2", "row12값3", "row12값4", "row12값5", "row12값6", "row12값7", "row12값8", "row12값9", "row12값10", "row12값11", "row12값12"],
+    ["row13값1", "row13값2", "row13값3", "row13값4", "row13값5", "row13값6", "row13값7", "row13값8", "row13값9", "row13값10", "row13값11", "row13값12"],
+    ["row14값1", "row14값2", "row14값3", "row14값4", "row14값5", "row14값6", "row14값7", "row14값8", "row14값9", "row14값10", "row14값11", "row14값12"],
+    ["row15값1", "row15값2", "row15값3", "row15값4", "row15값5", "row15값6", "row15값7", "row15값8", "row15값9", "row15값10", "row15값11", "row15값12"],
+    ["row16값1", "row16값2", "row16값3", "row16값4", "row16값5", "row16값6", "row16값7", "row16값8", "row16값9", "row16값10", "row16값11", "row16값12"],
+    ["row17값1", "row17값2", "row17값3", "row17값4", "row17값5", "row17값6", "row17값7", "row17값8", "row17값9", "row17값10", "row17값11", "row17값12"],
+    ["row18값1", "row18값2", "row18값3", "row18값4", "row18값5", "row18값6", "row18값7", "row18값8", "row18값9", "row18값10", "row18값11", "row18값12"],
+    ["row19값1", "row19값2", "row19값3", "row19값4", "row19값5", "row19값6", "row19값7", "row19값8", "row19값9", "row19값10", "row19값11", "row19값12"],
+    ["row20값1", "row20값2", "row10값3", "row20값4", "row20값5", "row20값6", "row20값7", "row20값8", "row20값9", "row20값10", "row20값11", "row20값12"],
+    ]
     
-    var records: [[String]] = (0 ..< 50).map { row in
-        (0 ..< 6).map {
-            column in
-            "Row \(row) columnssssssssdfdfaddffa \(column)"
-        }
-    }
-
+    
     var cellWidths: [CGFloat] = [ 180, 200, 180, 160, 200, 200 ]
     
     //JSon을 받아와 parsing한 뒤,
-    private var JsonRowCount: Int = 0
-    private var JsonColumnCount: Int = 0
+//    private var JsonRowCount: Int = 0
+//    private var JsonColumnCount: Int = 0
     
     private var isClickedArr: [[Bool]] = []
    
     //TODO: dataTitles를 가져오는 api 항목에 맞게 따로 불러오는 기능 구현해야함
     private var dataTitles: [String] = ["cur_unit", "ttb", "tts", "deal_bas_r", "bkpr", "yy_efee_r", "ten_dd_efee_r", "kftc_bkpr", "kftc_deal_bas_r", "cur_nm", "cur_unit", "ttb", "tts", "deal_bas_r", "bkpr", "yy_efee_r", "ten_dd_efee_r", "kftc_bkpr", "kftc_deal_bas_r", "cur_nm"]
     private var erData: [ExchangeRateCellData] = []
-
-    
-    
     
     private lazy var collectionView: UICollectionView = {
         let layout = GridLayout()
         layout.cellHeight = 44
-        layout.cellWidths = cellWidths
+        layout.cellWidths = Array(repeating: CGFloat(200), count: jsonResultArr[0].count + 1)
         //layout.sectionInset = UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2)
 //        layout.minimumLineSpacing = 4
 //        layout.minimumInteritemSpacing = 2
@@ -328,12 +349,12 @@ class ShowDataViewController: UIViewController {
 extension ShowDataViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     func numberOfSections(in collectionView: UICollectionView) -> Int {
 //        return records.count
-        return self.JsonRowCount + 1
+        return self.jsonResultArr.count
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 //        return records[section].count
-        return self.JsonColumnCount + 1
+        return self.jsonResultArr[0].count + 1
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -341,8 +362,17 @@ extension ShowDataViewController: UICollectionViewDataSource, UICollectionViewDe
         let isFirstRow: Bool = (indexPath.section == 0)
         let isFirstColumn: Bool = (indexPath.row == 0)
         
+        
+        if !isFirstColumn{
+            let now_title: String = jsonResultArr[indexPath.section][indexPath.row - 1]
+            cell.setup(isFirstRow: isFirstRow, isFirstColumn: isFirstColumn, title: now_title)
+//            cell.setup(isFirstRow: isFirstRow, isFirstColumn: isFirstColumn, title: "sct = " + String(indexPath.section) + "idx = " + String(indexPath.item))
+        }
+        else{
+            cell.setup(isFirstRow: isFirstRow, isFirstColumn: isFirstColumn, title: String(indexPath.section))
+        }
 //        cell.setRecord(records[indexPath.section][indexPath.item])
-        cell.setup(isFirstRow: isFirstRow, isFirstColumn: isFirstColumn, title: "sct = " + String(indexPath.section) + "idx = " + String(indexPath.item))
+       
 
         return cell
     }
@@ -450,13 +480,13 @@ extension ShowDataViewController{
                 print( "총 row 수 = " + String(self.erData.count))
                 print( "0번째 인덱스 " )
                 print( self.erData[0] )
-                self.JsonRowCount = self.erData.count
-                self.JsonColumnCount = 20
-                self.isClickedArr = Array(repeating: Array(repeating: false ,count: self.JsonColumnCount), count: self.JsonRowCount)
-                print("isClickedArr row길이 =")
-                print( self.isClickedArr.count)
-                print("isClickedArr column길이 =")
-                print( self.isClickedArr[0].count)
+//                self.JsonRowCount = self.erData.count
+//                self.JsonColumnCount = 20
+//                self.isClickedArr = Array(repeating: Array(repeating: false ,count: self.JsonColumnCount), count: self.JsonRowCount)
+//                print("isClickedArr row길이 =")
+//                print( self.isClickedArr.count)
+//                print("isClickedArr column길이 =")
+//                print( self.isClickedArr[0].count)
 
                 //테이블 뷰 다시 그려줌
                 self.collectionView.reloadData()
