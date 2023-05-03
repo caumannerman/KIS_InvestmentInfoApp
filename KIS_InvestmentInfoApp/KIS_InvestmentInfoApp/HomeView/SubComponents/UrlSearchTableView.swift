4ac38809e49db74c211aebdda6aaf334b5b9e038
caumannerman <uigohiiming2@gmail.com>
2023-05-03 14:36:34 +0900

    UrlSearchTableView 작성

--- KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/HomeView/SubComponents/UrlSearchTableView.swift
+++ KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/HomeView/SubComponents/UrlSearchTableView.swift
@@ -9,12 +9,49 @@ import UIKit
 
 class UrlSearchTableView: UITableView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private var urlsAlias: [String] = []
+    private var urlsArr: [String] = []
+    
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        attribute()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func attribute(){
+        self.dataSource = self
+        self.delegate = self
+        self.register(UrlSearchTableViewCell.self, forCellReuseIdentifier: "UrlSearchTableViewCell")
+        self.rowHeight = 100
+        self.backgroundColor = UIColor(patternImage: UIImage(named: "splash")!)
+        
+        self.urlsArr = UserDefaults.standard.array(forKey: "urls") as? [String] ?? ["정보가 없습니다"]
+        self.urlsAlias = UserDefaults.standard.array(forKey: "urlAlias") as? [String] ?? ["정보가 없습니다"]
+    }
+}
 
+extension UrlSearchTableView: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Did click cell!" + "\(indexPath)")
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+}
+
+extension UrlSearchTableView: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return urlsArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "UrlSearchTableViewCell", for: indexPath) as? UrlSearchTableViewCell else { return UITableViewCell()}
+        cell.selectionStyle = .none
+        cell.setup(title: urlsAlias[indexPath.row], url: urlsArr[indexPath.row])
+        return cell
+    }
 }
