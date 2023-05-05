4be38a6bf5cf0fed3f715fc0ff75a300cc6b1138
Yang <75043852+caumannerman@users.noreply.github.com>
2023-05-06 02:04:16 +0900

    [ShowDataVC]데이터 호출, csv저장 버튼 디자인 변경

--- KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/ShowDataView/ShowDataViewController.swift
+++ KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/ShowDataView/ShowDataViewController.swift
@@ -28,29 +28,7 @@ class ShowDataViewController: UIViewController {
     // ----------------------------------------------------------------------------- 임시 --------------------------------------------------------------------------- //
     
     // 이것이 json String을 이용해 최종적으로 얻은 배열이라고 생각하고 개발중
-    private var jsonResultArr: [[String]] = [
-    ["col이름1", "col이름2", "col이름3", "col이름4", "col이름5", "col이름6", "col이름7", "col이름8", "col이름9", "col이름10", "col이름11", "col이름12"],
-    ["row1값1", "row1값2", "row1값3", "row1값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
-    ["row2값1", "row2값2", "row2값3", "row2값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
-    ["row3값1", "row3값2", "row3값3", "row3값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
-    ["row4값1", "row4값2", "row4값3", "row4값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
-    ["row5값1", "row5값2", "row5값3", "row5값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
-    ["row6값1", "row6값2", "row6값3", "row6값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
-    ["row7값1", "row7값2", "row7값3", "row7값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
-    ["row8값1", "row8값2", "row8값3", "row8값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
-    ["row9값1", "row9값2", "row9값3", "row9값4", "row1값5", "row1값6", "row1값7", "row1값8", "row1값9", "row1값10", "row1값11", "row1값12"],
-    ["row10값1", "row10값2", "row10값3", "row10값4", "row10값5", "row10값6", "row10값7", "row10값8", "row10값9", "row10값10", "row10값11", "row10값12"],
-    ["row11값1", "row11값2", "row11값3", "row11값4", "row11값5", "row11값6", "row11값7", "row11값8", "row11값9", "row11값10", "row11값11", "row11값12"],
-    ["row12값1", "row12값2", "row12값3", "row12값4", "row12값5", "row12값6", "row12값7", "row12값8", "row12값9", "row12값10", "row12값11", "row12값12"],
-    ["row13값1", "row13값2", "row13값3", "row13값4", "row13값5", "row13값6", "row13값7", "row13값8", "row13값9", "row13값10", "row13값11", "row13값12"],
-    ["row14값1", "row14값2", "row14값3", "row14값4", "row14값5", "row14값6", "row14값7", "row14값8", "row14값9", "row14값10", "row14값11", "row14값12"],
-    ["row15값1", "row15값2", "row15값3", "row15값4", "row15값5", "row15값6", "row15값7", "row15값8", "row15값9", "row15값10", "row15값11", "row15값12"],
-    ["row16값1", "row16값2", "row16값3", "row16값4", "row16값5", "row16값6", "row16값7", "row16값8", "row16값9", "row16값10", "row16값11", "row16값12"],
-    ["row17값1", "row17값2", "row17값3", "row17값4", "row17값5", "row17값6", "row17값7", "row17값8", "row17값9", "row17값10", "row17값11", "row17값12"],
-    ["row18값1", "row18값2", "row18값3", "row18값4", "row18값5", "row18값6", "row18값7", "row18값8", "row18값9", "row18값10", "row18값11", "row18값12"],
-    ["row19값1", "row19값2", "row19값3", "row19값4", "row19값5", "row19값6", "row19값7", "row19값8", "row19값9", "row19값10", "row19값11", "row19값12"],
-    ["row20값1", "row20값2", "row10값3", "row20값4", "row20값5", "row20값6", "row20값7", "row20값8", "row20값9", "row20값10", "row20값11", "row20값12"],
-    ]
+    private var jsonResultArr: [[String]] = DummyClass.getJsonResultArr()
     
     private var isClickedArr_row: [Bool] = [true, false, false, true, false, false, false, true, true, false, true, false, false, false, true, false, true, false, false, true]
     private var isClickedArr_col: [Bool] = [false, false,true, true, false, false, false, true, false, false, true, true, ]
@@ -101,83 +79,9 @@ class ShowDataViewController: UIViewController {
     }()
     
     
-//    private lazy var pageButton: UIButton = {
-//        let button = UIButton()
-//        button.setTitle("1", for: .normal)
-//        button.backgroundColor = UIColor(red: 195/255.0, green: 222/255.0, blue: 194/255.0, alpha: 1.0)
-//        button.layer.borderColor = UIColor(red: 153/255.0, green: 76/255.0, blue: 0/255.0, alpha: 1.0).cgColor
-//        button.layer.borderWidth = 1.0
-//        button.layer.cornerRadius = 8.0
-//        button.addTarget(self, action: #selector(pageChangeButtonClicked), for: .touchUpInside)
-//
-//        return button
-//    }()
-//
-//    private lazy var pageButton2: UIButton = {
-//        let button = UIButton()
-//        button.setTitle("2", for: .normal)
-//        button.backgroundColor = UIColor(red: 195/255.0, green: 222/255.0, blue: 194/255.0, alpha: 1.0)
-//        button.layer.borderColor = UIColor(red: 153/255.0, green: 76/255.0, blue: 0/255.0, alpha: 1.0).cgColor
-//        button.layer.borderWidth = 1.0
-//        button.layer.cornerRadius = 8.0
-//        button.addTarget(self, action: #selector(pageChangeButtonClicked), for: .touchUpInside)
-//
-//        return button
-//    }()
-//
-//
-//    private lazy var pageButton3: UIButton = {
-//        let button = UIButton()
-//        button.setTitle("3", for: .normal)
-//        button.backgroundColor = UIColor(red: 195/255.0, green: 222/255.0, blue: 194/255.0, alpha: 1.0)
-//        button.layer.borderColor = UIColor(red: 153/255.0, green: 76/255.0, blue: 0/255.0, alpha: 1.0).cgColor
-//        button.layer.borderWidth = 1.0
-//        button.layer.cornerRadius = 8.0
-//        button.addTarget(self, action: #selector(pageChangeButtonClicked), for: .touchUpInside)
-//
-//        return button
-//    }()
-//
-    
-    private lazy var callButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Call", for: .normal)
-        button.backgroundColor = UIColor(red: 0/255.0, green: 204/255.0, blue: 244/255.0, alpha: 1.0)
-        button.layer.borderColor = UIColor(red: 153/255.0, green: 76/255.0, blue: 0/255.0, alpha: 1.0).cgColor
-        button.layer.borderWidth = 1.0
-        button.layer.cornerRadius = 10.0
-        button.addTarget(self, action: #selector(getfunc), for: .touchUpInside)
-        
-        return button
-    }()
-    
-    private lazy var saveButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Save", for: .normal)
-        button.backgroundColor = UIColor(red: 0/255.0, green: 204/255.0, blue: 244/255.0, alpha: 1.0)
-        button.layer.borderColor = UIColor(red: 153/255.0, green: 76/255.0, blue: 0/255.0, alpha: 1.0).cgColor
-        button.layer.borderWidth = 1.0
-        button.layer.cornerRadius = 10.0
-        button.addTarget(self, action: #selector(savefunc), for: .touchUpInside)
-        
-        return button
-    }()
-    
-   
-    
-//    private lazy var collectionView: UICollectionView = {
-//        let layout = UICollectionViewFlowLayout()
-//        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-//
-////        collectionView.delegate = self
-////        collectionView.dataSource = self
-//        collectionView.backgroundColor = .systemPink
-//
-//        collectionView.register(ButtonListViewCell.self, forCellWithReuseIdentifier: "ButtonListViewCell")
-//        return collectionView
-//    }()
-//
- 
+    private let get_save_view = UIView()
+    private lazy var callDataButton = UIButton()
+    private lazy var saveCsvButton = UIButton()
     
     // ------------------------------ UI Components ------------------------------ //
     
@@ -313,30 +217,31 @@ class ShowDataViewController: UIViewController {
     
     func attribute(){
         view.backgroundColor = .systemBackground
-//        tableView.backgroundColor = .lightGray
-//        collectionView.backgroundColor = .systemYellow
+
+        get_save_view.backgroundColor = UIColor(red: 236/255.0, green: 236/255.0, blue: 236/255.0, alpha: 1.0)
+        
+        callDataButton.backgroundColor = .white
+        callDataButton.setTitle("데이터 불러오기", for: .normal)
+        callDataButton.titleLabel?.font = .systemFont(ofSize: 28, weight: .bold)
+        callDataButton.setTitleColor(.black, for: .normal)
+        callDataButton.addTarget(self, action: #selector(getfunc), for: .touchUpInside)
+        callDataButton.layer.cornerRadius = 12.0
+        callDataButton.layer.borderWidth = 3.0
+        callDataButton.layer.borderColor = UIColor.lightGray.cgColor
+        
+        saveCsvButton.backgroundColor = .white
+        saveCsvButton.setTitle("저장하기", for: .normal)
+        saveCsvButton.titleLabel?.font = .systemFont(ofSize: 28, weight: .bold)
+        saveCsvButton.setTitleColor(.black, for: .normal)
+        saveCsvButton.addTarget(self, action: #selector(savefunc), for: .touchUpInside)
+        saveCsvButton.layer.cornerRadius = 12.0
+        saveCsvButton.layer.borderWidth = 3.0
+        saveCsvButton.layer.borderColor = UIColor.lightGray.cgColor
     }
     
     func layout(){
-//        view.addSubview(scrollView)
-//        scrollView.snp.makeConstraints{
-//            $0.top.equalTo(view.safeAreaLayoutGuide)
-////            $0.bottom.leading.trailing.equalToSuperview()
-//            $0.leading.trailing.equalToSuperview()
-//            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(100)
-//        }
-//        scrollView.addSubview(contentView)
-//        contentView.snp.makeConstraints{
-//            $0.edges.equalToSuperview()
-//            //세로를 고정시켜주어 세로스크롤 뷰가 된다.
-//            $0.height.equalToSuperview()
-//        }
-//        contentView.addSubview(stackView)
-//        stackView.snp.makeConstraints{
-//            $0.edges.equalToSuperview()
-//        }
         
-        [collectionView].forEach{
+        [collectionView, get_save_view].forEach{
             view.addSubview($0)
 //            stackView.addArrangedSubview($0)
 //            stackView.addSubview($0)
@@ -352,81 +257,27 @@ class ShowDataViewController: UIViewController {
             $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(100)
 //            $0.width.equalTo(scrollView.snp.width)
         }
-        
-//        tableView.snp.makeConstraints{
-////            $0.edges.equalToSuperview()
-//
-////            $0.top.equalTo(getDataButton.snp.bottom)
-//            $0.top.equalTo(view.safeAreaLayoutGuide)
-//            $0.leading.equalToSuperview()
-//            $0.trailing.equalToSuperview()
-//            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(100)
-////            $0.width.equalTo(scrollView.snp.width)
-//        }
-//
-        [callButton, saveButton].forEach{
-            view.addSubview($0)
+        get_save_view.snp.makeConstraints{
+            $0.top.equalTo(collectionView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(100)
+        }
+
+        [callDataButton, saveCsvButton].forEach{
+            get_save_view.addSubview($0)
+        }
+
+        callDataButton.snp.makeConstraints{
+            $0.top.bottom.equalToSuperview().inset(6)
+            $0.leading.equalToSuperview().inset(10)
+            $0.width.equalTo((UIScreen.main.bounds.width - 40) / 2 )
         }
         
-//        pageButton.snp.makeConstraints{
-//            $0.top.equalTo(collectionView.snp.bottom).offset(10)
-////            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(30)
-//            $0.leading.equalToSuperview().inset(60)
-//            $0.width.equalTo(20)
-//            $0.height.equalTo(20)
-//        }
-//
-//        pageButton2.snp.makeConstraints{
-//            $0.top.equalTo(collectionView.snp.bottom).offset(10)
-////            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(30)
-//            $0.leading.equalTo(pageButton.snp.trailing).offset(6)
-//            $0.width.equalTo(20)
-//            $0.height.equalTo(20)
-//        }
-//
-//        pageButton3.snp.makeConstraints{
-//            $0.top.equalTo(collectionView.snp.bottom).offset(10)
-////            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(30)
-//            $0.leading.equalTo(pageButton2 .snp.trailing).offset(6)
-//            $0.width.equalTo(20)
-//            $0.height.equalTo(20)
-//        }
-        callButton.snp.makeConstraints{
-            $0.top.equalTo(collectionView.snp.bottom).offset(12)
-            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(10)
-            $0.leading.equalToSuperview().inset(60)
-            $0.width.equalTo(120)
-//            $0.height.equalTo(40)
+        saveCsvButton.snp.makeConstraints{
+            $0.top.bottom.equalToSuperview().inset(6)
+            $0.trailing.equalToSuperview().inset(10)
+            $0.width.equalTo((UIScreen.main.bounds.width - 40) / 2 )
         }
-        
-        saveButton.snp.makeConstraints{
-            $0.top.equalTo(collectionView.snp.bottom).offset(12)
-            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(10)
-            $0.trailing.equalToSuperview().inset(60)
-            $0.width.equalTo(120)
-//            $0.height.equalTo(40)
-        }
-//        callButton.snp.makeConstraints{
-//            $0.top.equalTo(tableView.snp.bottom).offset(20)
-//            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(30)
-//            $0.leading.equalToSuperview().inset(60)
-//            $0.width.equalTo(120)
-////            $0.height.equalTo(40)
-//        }
-//
-//        saveButton.snp.makeConstraints{
-//            $0.top.equalTo(tableView.snp.bottom).offset(20)
-//            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(30)
-//            $0.trailing.equalToSuperview().inset(60)
-//            $0.width.equalTo(120)
-////            $0.height.equalTo(40)
-//        }
-        
-//        collectionView.snp.makeConstraints{
-//            $0.top.equalTo(tableView.snp.bottom)
-//            $0.leading.trailing.equalToSuperview()
-//            $0.height.equalTo(500)
-//        }
     }
 
     @objc func getfunc(){
