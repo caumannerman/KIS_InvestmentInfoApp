5f9ba6652150531b258d796f98b95426150b0815
caumannerman <uigohiiming2@gmail.com>
2023-05-12 16:46:32 +0900

    컬렉션뷰 Drag&Drop 기능 구현완료

--- KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/HomeView/SubComponents/MarketInfo/MarketCollectionView.swift
+++ KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/HomeView/SubComponents/MarketInfo/MarketCollectionView.swift
@@ -39,6 +39,9 @@ class MarketCollectionView: UICollectionView {
         self.isPagingEnabled = true
         self.dataSource = self
         self.delegate = self
+        self.dragDelegate = self
+        self.dropDelegate = self
+        self.dragInteractionEnabled = true
     }
     
 }
@@ -57,11 +60,82 @@ extension MarketCollectionView: UICollectionViewDataSource{
 
 extension MarketCollectionView: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: ( UIScreen.main.bounds.width - 40 ) / 3, height: ( UIScreen.main.bounds.width - 40 ) / 3)
+        switch indexPath.row {
+        case 0:
+            return CGSize(width: ( UIScreen.main.bounds.width - 40 ) / 3, height: ( UIScreen.main.bounds.width - 40 ) / 3)
+            
+        case 1:
+            return CGSize(width: ( UIScreen.main.bounds.width * 2 / 3 - 50 / 3 ) , height: ( UIScreen.main.bounds.width - 40 ) / 3)
+        case 2, 3, 4:
+            return CGSize(width: ( UIScreen.main.bounds.width - 40 ) / 3, height: ( UIScreen.main.bounds.width - 40 ) / 6)
+        case 6:
+            return CGSize(width: ( UIScreen.main.bounds.width - 40 ) / 3, height: ( UIScreen.main.bounds.width - 40 ) / 3)
+        case 5:
+            return CGSize(width: ( UIScreen.main.bounds.width - 40 ) / 3, height: ( UIScreen.main.bounds.width - 40 ) / 3)
+        default:
+            return CGSize(width: ( UIScreen.main.bounds.width - 40 ) / 3, height: ( UIScreen.main.bounds.width - 40 ) / 3)
+        }
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print(contents[indexPath.row])
         print("Clicked CV cell")
+    }
+}
+
+
+extension MarketCollectionView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        true
+    }
+    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        
+    }
+}
+
+extension MarketCollectionView: UICollectionViewDropDelegate {
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+            var destinationIndexPath: IndexPath
+            if let indexPath = coordinator.destinationIndexPath {
+                destinationIndexPath = indexPath
+            } else {
+                let row = collectionView.numberOfItems(inSection: 0)
+                destinationIndexPath = IndexPath(item: row - 1, section: 0)
+            }
+            
+            if coordinator.proposal.operation == .move {
+                reorderItems(coordinator: coordinator, destinationIndexPath: destinationIndexPath, collectionView: collectionView)
+            }
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+            if collectionView.hasActiveDrag {
+                return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+            }
+            return UICollectionViewDropProposal(operation: .forbidden)
+        }
+        
+        private func reorderItems(coordinator: UICollectionViewDropCoordinator, destinationIndexPath: IndexPath, collectionView: UICollectionView) {
+            if
+                let item = coordinator.items.first,
+                let sourceIndexPath = item.sourceIndexPath {
+                collectionView.performBatchUpdates({
+                    let temp = contents[sourceIndexPath.item]
+                    contents.remove(at: sourceIndexPath.item)
+                    contents.insert(temp, at: destinationIndexPath.item)
+                    collectionView.deleteItems(at: [sourceIndexPath])
+                    collectionView.insertItems(at: [destinationIndexPath])
+                }) { done in
+                    //
+                }
+                coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
+            }
+        }
+}
+
+extension MarketCollectionView: UICollectionViewDragDelegate {
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        return []
     }
 }
