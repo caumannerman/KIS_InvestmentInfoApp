8783fe14e53bc72dee5941e5fc37ca3a239b6a25
caumannerman <uigohiiming2@gmail.com>
2023-05-23 16:12:30 +0900

    MarketInfoData url 분류 주석 추가

--- KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/State/MarketInfoData.swift
+++ KIS_InvestmentInfoApp/KIS_InvestmentInfoApp/State/MarketInfoData.swift
@@ -7,6 +7,70 @@
 
 import Foundation
 
+//1. 금융위원회_주식시세정보
+// 
+//apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService
+//
+//    (1) 주식시세  /getStockPriceInfo
+//
+//    (2) 신주인수권증서시세 /getPreemptiveRightCertificatePriceInfo
+//
+//    (3) 수익증권시세 /getSecuritiesPriceInfo
+//
+//    (4) 신주인수권증권시세 /getPreemptiveRightSecuritiesPriceInfo
+//
+//
+//
+//2. 금융위원회_지수시세정보
+//
+//apis.data.go.kr/1160100/service/GetMarketIndexInfoService
+//
+//    (1) 주가지수시세 /getStockMarketIndex
+//
+//    (2) 채권지수시세 /getBondMarketIndex
+//
+//    (3) 파생상품지수시세 /getDerivationProductMarketIndex
+//
+//
+//
+//
+//3. 금융위원회_일반상품시세정보
+//
+//apis.data.go.kr/1160100/service/GetGeneralProductInfoService
+//
+//    (1) 석유시세 /getOilPriceInfo
+//
+//    (2) 금시세 / /getGoldPriceInfo
+//
+//    (3) 배출권시세 /getCertifiedEmissionReductionPriceInfo
+//
+//
+//
+//4. 금융위원회_증권상품시세정보
+//
+//apis.data.go.kr/1160100/service/GetSecuritiesProductInfoService
+//
+//    (1) ETF시세 /getETFPriceInfo
+//
+//    (2) ETN시세 /getETNPriceInfo
+//
+//    (3) ELW시세 /getELWPriceInfo
+//
+//
+//5. 금융위원회_채권시세정보
+//
+//apis.data.go.kr/1160100/service/GetBondSecuritiesInfoService
+//
+//    (1) 채권시세 /getBondPriceInfo
+//
+//
+//6. 금융위원회_파생상품시세정보
+//
+//apis.data.go.kr/1160100/service/GetDerivativeProductInfoService
+//
+//    (1) 선물시세 /getStockFuturesPriceInfo
+//
+//    (2) 옵션시세 /getOptionsPriceInfo
 class MarketInfoData {
     
     // 앱 실행중에 동적으로 변하지 않는 값이다.
